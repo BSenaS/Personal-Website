30c3b051b094816e952e6d7184b356c8a291be43
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-01-31 23:11:40 +0300

    Basic mock data created Assets added.

---
+++ src/data.js
@@ -0,0 +1,181 @@
+export const data = {
+  tr: {
+    header: {
+      darkmode: "GECE MODU",
+      lightmode: "GÜNDÜZ MODU",
+      languagetxt: "İNGİLİZCEYE",
+      language: " GEÇ",
+    },
+    introduction: {
+      greeting1: "Merhaba,ben ",
+      greeting2: "Batuhan Sena Sert",
+      greeting3: "Web Developer",
+      p1: "Merhaba,ben Batuhan.Bir web geliştiriciyim. Sağlam ve ölçeklenebilir ön uç ürünleri oluşturmak ve harika kullanıcı deneyimleri sunmak için buradayım. Benimle tanışmak isterseniz, aşağıdaki kanallardan bana ulaşabilirsiniz.",
+      iletişim1: "batuhansenasert@gmail.com",
+      iletişim2: "https://github.com/BSenaS",
+      iletişim3: "https://www.linkedin.com/in/bssert/",
+    },
+    skills: {
+      title: "Beceriler",
+      skillerim: [
+        { name: "Java Script", desc: "LoremIpsum1" },
+        { name: "React.Js", desc: "LoremIpsum2" },
+        { name: "Node.Js", desc: "LoremIpsum3" },
+      ],
+    },
+    profile: {
+      title: "Profil",
+      basicinfo: {
+        birthdatekey: "Doğum Tarihi",
+        birthdate: "20.11.1997",
+        locationkey: "İkamet Şehri",
+        location: "Muğla",
+        edukey: "Eğitim Durumu",
+        edu: "Akdeniz Ünv. Yönetim Bilişim Sistemleri Lisans, 2023",
+        rolekey: "Tercih Ettiği Rol",
+        role: "Frontend, UI",
+      },
+      aboutme: {
+        title: "Hakkımda",
+        p1: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+        p2: "Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam! ",
+      },
+    },
+    projects: {
+      title: "Projeler",
+      projectarray: [1, 2, 3],
+      project1: {
+        title: "Pizza Siparişi",
+        p1: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        tags: ["JS", "React", "Vercel", "Axios", "Router", "Context"],
+        links: {
+          git: "Github",
+          redirect: "Siteye git ",
+        },
+        link: "linkgelecek.com",
+        image: "/assets/projeResim1.png",
+      },
+      project2: {
+        title: "Redux Filmler",
+        p1: "Bu projede, ilginizi çeken filmin detaylarını görüntüleyebilir, değiştirmek istediğiniz bilgileri değiştirebilir ve yeni bir film ekleyebilirsiniz. Ek olarak, en sevdiğiniz filmleri favoriler listesine ekleyebilirsiniz. Bileşenler arasındaki veri aktarımı, Redux kitaplığı kullanılarak gerçekleştirilir.",
+        tags: ["react", "redux", "axios", "router", "vercel"],
+        links: {
+          git: "Github'da göster",
+          redirect: "Uygulamaya git ",
+        },
+        link: "linkgelecek.com",
+        image: "/assets/projeResim2.png",
+      },
+      project3: {
+        title: "Redux Filmler",
+        p1: "Bu projede, ilginizi çeken filmin detaylarını görüntüleyebilir, değiştirmek istediğiniz bilgileri değiştirebilir ve yeni bir film ekleyebilirsiniz. Ek olarak, en sevdiğiniz filmleri favoriler listesine ekleyebilirsiniz. Bileşenler arasındaki veri aktarımı, Redux kitaplığı kullanılarak gerçekleştirilir.",
+        tags: ["react", "redux", "axios", "router", "vercel"],
+        links: {
+          git: "Github'da göster",
+          redirect: "Uygulamaya git ",
+        },
+        link: "linkgelecek.com",
+        image: "/assets/projeResim3.png",
+      },
+    },
+    footer: {
+      title: "Bir sonraki ürününüz üzerinde birlikte çalışalım.",
+      links: {
+        git: "Github",
+        blog: "Kişisel Blog",
+        linkedin: "Linkedin",
+        email: "Email",
+      },
+    },
+  },
+  en: {
+    header: {
+      darkmode: "DARK MODE",
+      lightmode: "LIGHT MODE",
+      language: "SWITCH TO",
+      languagetxt: " TURKISH",
+    },
+    introduction: {
+      greeting1: "Hi, I'm ",
+      greeting2: "Batuhan Sena Sert",
+      greeting3: "Web Developer",
+      p1: "Hi, I'm Batuhan. I'm a web developer. I'm here to create robust and scalable frontend products and provide great user experiences. If you'd like to get to know me, you can reach me through the channels below.",
+      contact1: "batuhansenasert@gmail.com",
+      contact2: "https://github.com/BSenaS",
+      contact3: "https://www.linkedin.com/in/bssert/",
+    },
+    skills: {
+      title: "Skills",
+      skillset: [
+        { name: "JavaScript", desc: "LoremIpsum1" },
+        { name: "React.js", desc: "LoremIpsum2" },
+        { name: "Node.js", desc: "LoremIpsum3" },
+      ],
+    },
+    profile: {
+      title: "Profile",
+      basicinfo: {
+        birthdatekey: "Date of Birth",
+        birthdate: "20.11.1997",
+        locationkey: "City of Residence",
+        location: "Muğla",
+        edukey: "Education",
+        edu: "Akdeniz University, Management Information Systems Bachelor's Degree, 2023",
+        rolekey: "Preferred Role",
+        role: "Frontend, UI",
+      },
+      aboutme: {
+        title: "About Me",
+        p1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aut odit laborum aliquam voluptatum nisi mollitia.",
+        p2: "Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+      },
+    },
+
+    projects: {
+      title: "Projeler",
+      projectarray: [1, 2],
+      project1: {
+        title: "Workintech",
+        p1: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        tags: ["JS", "React", "Vercel", "Axios", "Router", "Context"],
+        links: {
+          git: "Github",
+          redirect: "Siteye git ",
+        },
+        link: "linkgelecek.com",
+        image: "/assets/projeResim1.png",
+      },
+      project2: {
+        title: "Random Jokes",
+        p1: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        tags: ["react", "redux", "axios", "router", "vercel"],
+        links: {
+          git: "Github",
+          redirect: "Uygulamaya git ",
+        },
+        link: "linkgelecek.com",
+        image: "/assets/projeResim2.png",
+      },
+      project3: {
+        title: "Journey",
+        p1: "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        tags: ["react", "redux", "axios", "router", "vercel"],
+        links: {
+          git: "Github",
+          redirect: "Uygulamaya git ",
+        },
+        link: "linkgelecek.com",
+        image: "/assets/projeResim3.png",
+      },
+    },
+    footer: {
+      title: "Let's work together on your next product.",
+      links: {
+        git: "Github",
+        blog: "Personal Blog",
+        linkedin: "Linkedin",
+        email: "batuhansenasert@gmail.com",
+      },
+    },
+  },
+};
