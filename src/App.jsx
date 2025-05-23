2e74860094ac74db2574a604f0ecbd1e519504b7
Batuhan Sena Sert <batuhansenasert@gmail.com>
2025-05-23 16:07:51 +0300

    -Api fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,23 @@ import { MainLayout } from "./layout/MainLayout.jsx";
 
 export function App() {
   const { setTextData, textData, setDarkMode } = useLang();
+
   useEffect(() => {
-    axios
-      .post("https://reqres.in/api/workintech", data)
-      .then((response) => {
-        //1.Postlanan veriyi, setTextDataya atayıp kullanma.
-        setTextData({ ...response.data });
-        //LS'den datayi çek, ve kullanıcı seçimini hatırla.
-        const theme = localStorage.getItem("theme");
-        theme === "dark" ? setDarkMode(true) : setDarkMode(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    setTextData(data);
+    const theme = localStorage.getItem("theme");
+    theme === "dark" ? setDarkMode(true) : setDarkMode(false);
+    // axios
+    //   .post("https://reqres.in/api/workintech", data)
+    //   .then((response) => {
+    //     //1.Postlanan veriyi, setTextDataya atayıp kullanma.
+    //     setTextData({ ...response.data });
+    //     //LS'den datayi çek, ve kullanıcı seçimini hatırla.
+    //     const theme = localStorage.getItem("theme");
+    //     theme === "dark" ? setDarkMode(true) : setDarkMode(false);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   }, []);
   //Apiden veri dönmeden componentleri render etme.Daha temiz bir kod yazılabilirmi?
   if (!Object.keys(textData).length) {
